c801a9094291c69af4e9d2f3cd071b5455cddddd
Juan Gálvez <jfgalvezparra@gmail.com>
2024-01-04 22:04:43 -0500

    ADD models entry and author

--- core/models.py
+++ core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(
+        max_length=30,
+        help_text = "Name of the author",
+        null = False,
+        blank = False
+    )
+    last_name = models.CharField(
+        max_length=30,
+        help_text = "Last name of the author",
+        null = False,
+        blank = False
+    )
+    nationality = models.CharField(max_length=30)
+    email = models.EmailField(
+        null=False,
+        blank = False,
+        unique = True
+    )
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now = True)
+
+class EntryBlog(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="entry_blog")
+    content = models.TextField()
+     
+
